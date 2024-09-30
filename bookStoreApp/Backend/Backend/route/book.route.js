b6e82ede70beaa92821376975b67575fa589bc8d
Ashish Kumar <121416134+codingbyash@users.noreply.github.com>
2024-09-30 23:45:49 +0530

    Delete bookStoreApp directory

--- bookStoreApp/Backend/Backend/route/book.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getBook } from "../controller/book.controller.js"; //getbook func controller se liya
-
-const router = express.Router();
-
-router.get("/", getBook); // jaise hi request fetch kare to hmara getBook function run hona chhiye
-
-export default router;
